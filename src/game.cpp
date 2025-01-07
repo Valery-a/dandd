607bbf53fded2ef2bd2d1d26e76f180db105a293
Valery-a <ValeryKulchicki@gmail.com>
2025-01-07 19:04:03 +0200

    add: simple demo game scene

--- src/game.cpp
+++ src/game.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 using namespace std;
 
+const int WIDTH = 10;
+const int HEIGHT = 10;
+
 struct Player {
     int x, y;
 };
@@ -14,12 +17,7 @@ struct GameState {
 
 GameState state;
 
-const int WIDTH = 10;
-const int HEIGHT = 10;
-const int MAX_LIVES = 3;
-
 void initializeMap() {
-
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             state.map[i][j] = ' ';
@@ -48,4 +46,53 @@ void initializeMap() {
         state.player.y = rand() % WIDTH;
     } while (state.map[state.player.x][state.player.y] != ' ');
     state.map[state.player.x][state.player.y] = '@';
+}
+
+void printMap() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            cout << state.map[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void movePlayer(char direction) {
+    int newX = state.player.x;
+    int newY = state.player.y;
+
+    switch (direction) {
+        case 'w':
+            newX--;
+            break;
+        case 's':
+            newX++;
+            break;
+        case 'a':
+            newY--;
+            break;
+        case 'd':
+            newY++;
+            break;
+    }
+
+    state.map[state.player.x][state.player.y] = ' ';
+    state.player.x = newX;
+    state.player.y = newY;
+    state.map[state.player.x][state.player.y] = '@';
+}
+
+int main() {
+    srand(time(0));
+    initializeMap();
+
+    while (true) {
+        printMap();
+        char input;
+        cin >> input;
+        movePlayer(tolower(input));
+    }
+
+    return 0;
 }
