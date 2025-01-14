f54209d002402d43d7b1ed7557ada6d86b36414a
Valery-a <ValeryKulchicki@gmail.com>
2025-01-14 23:21:04 +0200

    fix: optimised profile saving and loading

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,49 @@
 
 using namespace std;
 
-void saveLevel(const char* username, int level, int coins, int lives) {
-    char filename[100];
-    strcpy(filename, username);
-    strcat(filename, "_progress.txt");
+struct PlayerProfile {
+    char username[64];
+    int mapChoice;
+    int lives;
+    int totalCoins;
+    int playerX;
+    int playerY;
+};
 
-    ofstream file(filename);
-    if (file.is_open()) {
-        file << level << endl;
-        file << coins << endl;
-        file << lives << endl;
-        file.close();
-        cout << "Your tarnished legacy has been inscribed upon the annals of fate, " << username << "." << endl;
-    } else {
-        cout << "The fingers tremble... the record cannot be written." << endl;
+void savePlayerProfile(const PlayerProfile &profile) {
+    char filename[128];
+    sprintf(filename, "%s.txt", profile.username);
+
+    ofstream fout(filename);
+    if(!fout.is_open()) {
+        return;
     }
+
+    fout << profile.username << endl
+        << profile.mapChoice << endl
+        << profile.lives << endl
+        << profile.totalCoins << endl
+        << profile.playerX << endl
+        << profile.playerY << endl;
+    fout.close();
 }
 
-bool loadLevel(const char* username, int &level, int &coins, int &lives) {
-    char filename[100];
-    strcpy(filename, username);
-    strcat(filename, "_progress.txt");
+bool loadPlayerProfile(const char* uname, PlayerProfile &profile) {
+    char filename[128];
+    sprintf(filename, "%s.txt", uname);
 
-    ifstream file(filename);
-    if (file.is_open()) {
-        file >> level;
-        file >> coins;
-        file >> lives;
-        file.close();
-        cout << "The remnants of your journey have been unearthed, Tarnished " << username << "." << endl;
-        return true;
-    } else {
-        cout << "No memory remains of your tarnished path, " << username << ". Begin anew." << endl;
+    ifstream fin(filename);
+    if(!fin.is_open()) {
         return false;
     }
+    fin >> profile.username
+        >> profile.mapChoice
+        >> profile.lives
+        >> profile.totalCoins
+        >> profile.playerX
+        >> profile.playerY;
+    fin.close();
+    return true;
 }
 
 void resetProgress(const char* username) {
@@ -56,7 +65,7 @@ void resetProgress(const char* username) {
 
 void exitGame(const char* username, int level, int coins, int lives) {
     cout << "The tarnished one rests, for now. Your journey has been recorded." << endl;
-    saveLevel(username, level, coins, lives);
+    //saveLevel(username, level, coins, lives);
     cout << "Fare thee well, " << username << ". May the grace of the Erdtree guide thee henceforth." << endl;
 }
 
@@ -77,11 +86,11 @@ int main() {
     cout << "Speak thy name, Tarnished one: ";
     cin >> username;
 
-    if (loadLevel(username, level, coins, lives)) {
-        cout << "Welcome back, " << username << ". Thou standest at Level " << level << ", with " << coins << " runes and " << lives << " vestiges of life remaining." << endl;
-    } else {
-        cout << "A new journey begins. Bear the burden of grace, " << username << "." << endl;
-    }
+    // if (loadLevel(username, level, coins, lives)) {
+    //     cout << "Welcome back, " << username << ". Thou standest at Level " << level << ", with " << coins << " runes and " << lives << " vestiges of life remaining." << endl;
+    // } else {
+    //     cout << "A new journey begins. Bear the burden of grace, " << username << "." << endl;
+    // }
 
     while (true) {
         menu();
@@ -111,7 +120,7 @@ int main() {
         if (lives > 0) lives--;
 
         cout << "Thy current plight: Level " << level << ", Runes " << coins << ", Vestiges of Life " << lives << "." << endl;
-        saveLevel(username, level, coins, lives);
+        //saveLevel(username, level, coins, lives);
     }
 
     return 0;
